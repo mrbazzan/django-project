a585e5202d2fd5831e489d9876fdc301ddf359b6
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-12-30 14:38:24 +0100

    blog: Cannonical url for models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -37,6 +38,9 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
 
     def __str__(self):
         return self.title
