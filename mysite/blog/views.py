483a1458a55d8300e05fec2b1e0760d4eaef5201
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-01-01 11:32:33 +0100

    blog: Sharing posts via email
    Implemented using:
    - Django Forms and
    - Django's send_mail function

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
 from django.http import Http404
 from .models import Post
 
@@ -32,3 +34,25 @@ def post_detail(request, year, month, day, post_slug):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    sent = False
+    post = get_object_or_404(Post.published, id=post_id)
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{form_data['name']} recommends you read "\
+                      f"<em>{post.title}</em>"
+            message = f"Read <em>{post.title}</em> at\n\t{post_url}\n\n"\
+                      f"{form_data['name']}'s comment: {form_data['comment']}"
+            send_mail(subject, message, '', [form_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'form': form, 'post': post, 'sent': sent})
