10c833a4e745050a4432be28a38c848871169b09
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-01-24 10:23:56 +0100

    blog: templatetags: inclusion_tag to show latest posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def total_posts():
     return Post.published.count()
 
+
+@register.inclusion_tag('blog/post/list_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'posts': latest_posts}
+
