67aa701376f2ae1ca5a57ad1672358b8146b5fb5
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-02-02 16:08:43 +0100

    blog: Add filter to support markdown syntax

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,9 +1,16 @@
 
+from django.utils.safestring import mark_safe
 from django.db.models import Count
 from django import template
 from ..models import Post
+import markdown
 
 register = template.Library()
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
 
 @register.simple_tag
 def total_posts():
