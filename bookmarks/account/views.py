148a694390fa08c2cb38257e091da50c66c4c6d1
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-02-20 10:48:16 +0100

    bookmark: Adding custom login view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            user = authenticate(request,
+                                username=form_data["username"],
+                                password=form_data["password"])
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
