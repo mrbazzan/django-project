323590542c6b30769d675de7bf7f70dc19b7acee
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-02-27 12:33:50 +0100

    bookmarks/account: Password reset functionality

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -120,6 +120,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
